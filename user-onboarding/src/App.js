7ac55ef4599f538fcb711c4c46bc096c62267c48
yirano <yirano@protonmail.ch>
2020-03-12 15:04:08 -0400

    Set up workspace for project - install axios, Formik, and Yup - create FormContainer component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FormContainer from './Components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <FormContainer />
     </div>
   );
 }
---
+++ user-onboarding/src/Components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withFormik, Form, Field } from 'formik'
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const FormContainer = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default FormContainer;
